246e466cd75725b54693882e0363f413fd2800ef
robinxdroid <robinxdroid@gmail.com>
2021-02-24 14:39:26 +0800

    ref |> 矩阵旋转

--- leetcode/src/main/java/com/github/leetcode/Application.kt
+++ leetcode/src/main/java/com/github/leetcode/Application.kt
@@ -13,13 +13,28 @@ object Application {
         val result = Introduction.findMiddleIndex(temp)
         println(result)
 
-        val searchResult = Introduction.searchInsert(intArrayOf(1, 3, 5, 6),5)
+        val searchResult = Introduction.searchInsert(intArrayOf(1, 3, 5, 6), 5)
         println(searchResult)
 
-        val intervals = arrayOf(intArrayOf(1,3), intArrayOf(2,6), intArrayOf(8,10), intArrayOf(15,18))
+        val intervals =
+            arrayOf(intArrayOf(1, 3), intArrayOf(2, 6), intArrayOf(8, 10), intArrayOf(15, 18))
         val mergerResult = Introduction.mergeArray(intervals)
         mergerResult.forEach {
-            println("intArray：[${it[0]},${it[1]}]")
+            println("mergerResult：${it.contentToString()}")
+        }
+
+        val matrix = arrayOf(
+            intArrayOf(1, 3, 5, 7),
+            intArrayOf(2, 6, 7, 8),
+            intArrayOf(8, 10, 12, 14),
+            intArrayOf(15, 18, 19, 20)
+        )
+        matrix.forEach {
+            println("旋转前matrix:${it.contentToString()}")
+        }
+        Introduction.rotate(matrix)
+        matrix.forEach {
+            println("旋转后matrix:${it.contentToString()}")
         }
 
     }
