24059c470e891aaf22aa61d498144524cfadccb7
Robin <robinxdroid@gmail.com>
2024-03-22 11:23:47 +0800

    MergLists算法优化，支持比对前前后后元素，增强重复准确性判定

--- leetcode/src/main/java/com/github/leetcode/MergeLists.java
+++ leetcode/src/main/java/com/github/leetcode/MergeLists.java
@@ -14,9 +14,8 @@ public class MergeLists {
     // 允许连续重复元素出现的次数，也是重复检测map池的数量
     public static final int dumplicateItemCheckPoolSize = 20;
 
-    public static List<String> mergeLists(List<String>... lists) {
-        // 创建双向链表的头节点
-        Node head = new Node("\0");
+    public static List<String> mergeLists(Node head, List<String>... lists) {
+
         Node tail = head;
 
         // 创建映射以跟踪每个元素的前一个和后一个元素
@@ -30,89 +29,108 @@ public class MergeLists {
             int preIndex = -1;
             int nextIndex = -1;
             for (int i = 0; i < sublist.size(); i++) {
+                preIndex = i - 1;
                 nextIndex = i + 1;
                 String ch = sublist.get(i);
 
-                boolean allow = true;
+                boolean nonRepetitive = true;
+
+                // 计算包含重复项的mapList数量
+                List<Map<String, Node>> containsMapList = new ArrayList<>();
+                for (Map<String, Node> map : mapList) {
+                    if (map.containsKey(ch)) {
+                        containsMapList.add(map);
+                    }
+                }
 
                 // 检查当前元素是否已经存在于链表中
+                int checkedCount = 0;
                 for (Map<String, Node> map : mapList) {
                     // 遍历每个重复子map中是否存在重复项，当前map中根据上下文没有匹配上重复项，就继续匹配下一个map，直到匹配上为止
                     Map<String, Node> targetMap = map;
                     boolean contains = targetMap.containsKey(ch);
                     if (contains) {
                         if (preIndex >= 0) {
-                            String preItem = sublist.get(preIndex);
 
                             if (nextIndex < sublist.size()) {
                                 // 需要比pre next
-                                String nextItem = sublist.get(nextIndex);
                                 Node mapItem = targetMap.get(ch);
-                                if (mapItem.prev != null && mapItem.next != null) {
-                                    if (preItem.equals(mapItem.prev.value) &&
-                                            nextItem.equals(mapItem.next.value)) {
-                                        allow = false;
-                                    }
-                                } else if (mapItem.prev != null) {
-                                    if (preItem.equals(mapItem.prev.value)) {
-                                        allow = false;
-                                    }
-                                } else if (mapItem.next != null) {
-                                    if (nextItem.equals(mapItem.next.value)) {
-                                        allow = false;
-                                    }
-                                } else {
-                                    // pre没比上，next也没比上，一般subList只包含一个元素，且在map中有重复项
-                                    // 这种情况暂时，归类为重复，因为没有上下文，不好界定是否真的是重复元素，先这么处理
-                                    allow = false;
+                                boolean duplicate = false;
+                                try {
+                                    duplicate = checkPreNextDumplicate(preIndex, nextIndex, sublist, mapItem);
+                                } catch (Exception e) {
                                 }
+
+                                if (duplicate) {
+                                    // 再次匹配目标的前前元素，后后元素
+
+                                    // 匹配前前
+                                    try {
+                                        duplicate = checkPreDumplicate(preIndex - 1, sublist, mapItem.prev);
+                                    } catch (Exception e) {
+                                    }
+
+                                    if (duplicate) {
+                                        // 匹配后后
+                                        try {
+                                            duplicate = checkNextDumplicate(nextIndex + 1, sublist, mapItem.next);
+                                        } catch (Exception e) {
+                                        }
+                                    }
+                                }
+
+                                nonRepetitive = !duplicate;
 
                             } else {
                                 // 需要比pre
                                 Node mapItem = targetMap.get(ch);
-                                if (mapItem.prev != null) {
-                                    if (preItem.equals(mapItem.prev.value)) {
-                                        allow = false;
+                                boolean duplicate = false;
+                                try {
+                                    duplicate = checkPreDumplicate(preIndex, sublist, mapItem);
+                                } catch (Exception e) {
+                                }
+                                if (duplicate) {
+                                    // 再次匹配目标的前前元素
+                                    try {
+                                        duplicate = checkPreDumplicate(preIndex - 1, sublist, mapItem.prev);
+                                    } catch (Exception e) {
                                     }
-                                } else {
-                                    allow = true;
                                 }
 
+                                nonRepetitive = !duplicate;
                             }
                         } else {
                             // 只要比next
-                            if (nextIndex < sublist.size()) {
-                                String nextItem = sublist.get(nextIndex);
-                                Node mapItem = targetMap.get(ch);
-                                if (mapItem.next != null) {
-                                    if (nextItem.equals(mapItem.next.value)) {
-                                        allow = false;
-                                    }
-                                } else {
-                                    allow = true;
-                                }
-
-                            } else {
-                                // pre没比上，next也没比上，一般subList只包含一个元素，且在map中有重复项
-                                // 这种情况暂时，归类为重复，因为没有上下文，不好界定是否真的是重复元素，先这么处理
-                                allow = false;
+                            Node mapItem = targetMap.get(ch);
+                            boolean duplicate = false;
+                            try {
+                                duplicate = checkNextDumplicate(nextIndex, sublist, mapItem);
+                            } catch (Exception e) {
                             }
+                            if (duplicate) {
+                                // 再次匹配目标的后后元素
+                                try {
+                                    duplicate = checkNextDumplicate(nextIndex + 1, sublist, mapItem.next);
+                                } catch (Exception e) {
+                                }
+                            }
+                            nonRepetitive = !duplicate;
                         }
 
 
                     } else {
-                        allow = true;
+                        nonRepetitive = true;
                     }
 
-                    if (allow == false) {
+                    if (nonRepetitive == false) {
                         // 根据上下文对比匹配到了重复项，跳出mapList重复项检索逻辑
                         break;
                     }
                 }
 
 
-                if (allow) {
-                    // 如果不存在，创建新节点并添加到链表尾部
+                if (nonRepetitive) {
+                    // 如果不重复，创建新节点并添加到链表尾部
                     Node newNode = new Node(ch);
                     tail.next = newNode;
                     newNode.prev = tail;
@@ -125,7 +143,6 @@ public class MergeLists {
                     }
 
                 }
-                preIndex = i;
             }
         }
 
@@ -140,8 +157,114 @@ public class MergeLists {
         return mergedList;
     }
 
+    private static boolean checkPreDumplicate(int preIndex, List<String> sublist, Node mapItem) throws RuntimeException {
+        if (mapItem == null) {
+            return true;
+        }
+        boolean duplicate = false;
+        if (preIndex >= 0 && preIndex < sublist.size()) {
+            String preItem = sublist.get(preIndex);
+            if (mapItem.prev != null) {
+                duplicate = preItem.equals(mapItem.prev.value);
+            } else {
+                throw new RuntimeException("此结果不需要参考，没有进行比较");
+            }
+        } else {
+            throw new RuntimeException("此结果不需要参考，没有进行比较");
+        }
+
+        return duplicate;
+    }
+
+    private static boolean checkNextDumplicate(int nextIndex, List<String> sublist, Node mapItem) throws RuntimeException {
+        if (mapItem == null) {
+            return true;
+        }
+        boolean duplicate = false;
+        if (nextIndex >= 0 && nextIndex < sublist.size()) {
+            String nextItem = sublist.get(nextIndex);
+
+            if (mapItem.next != null) {
+                duplicate = nextItem.equals(mapItem.next.value);
+            } else {
+                throw new RuntimeException("此结果不需要参考，没有进行比较");
+            }
+        } else {
+            throw new RuntimeException("此结果不需要参考，没有进行比较");
+        }
+        return duplicate;
+    }
+
+    private static boolean checkPreNextDumplicate(int preIndex, int nextIndex, List<String> sublist, Node mapItem) throws RuntimeException {
+        if (mapItem == null) {
+            return true;
+        }
+        boolean duplicate = false;
+        String preItem = sublist.get(preIndex);
+
+        /*String nextItem = sublist.get(nextIndex);
+        // 需要比pre next
+        if (mapItem.prev != null && mapItem.next != null) {
+            if (preItem.equals(mapItem.prev.value) &&
+                    nextItem.equals(mapItem.next.value)) {
+                duplicate = true;
+            }
+        } else if (mapItem.prev != null) {
+            if (preItem.equals(mapItem.prev.value)) {
+                duplicate = true;
+            }
+        } else if (mapItem.next != null) {
+            if (nextItem.equals(mapItem.next.value)) {
+                duplicate = true;
+            }
+        } else {
+            // pre没比上，next也没比上，一般subList只包含一个元素，且在map中有重复项
+            // 这种情况暂时，归类为重复，因为没有上下文，不好界定是否真的是重复元素，先这么处理
+            duplicate = true;
+        }*/
+
+        if (preIndex >= 0) {
+            if (nextIndex < sublist.size()) {
+                String nextItem = sublist.get(nextIndex);
+                // 需要比pre next
+                if (mapItem.prev != null && mapItem.next != null) {
+                    duplicate = preItem.equals(mapItem.prev.value) &&
+                            nextItem.equals(mapItem.next.value);
+                } else if (mapItem.prev != null) {
+                    duplicate = preItem.equals(mapItem.prev.value);
+                } else if (mapItem.next != null) {
+                    duplicate = nextItem.equals(mapItem.next.value);
+                } else {
+                    throw new RuntimeException("此结果不需要参考，没有进行比较");
+                }
+            } else {
+                // 需要比pre
+                if (mapItem.prev != null) {
+                    duplicate = preItem.equals(mapItem.prev.value);
+                } else {
+                    throw new RuntimeException("此结果不需要参考，没有进行比较");
+                }
+            }
+        } else {
+            // 只比next
+            if (nextIndex < sublist.size()) {
+                String nextItem = sublist.get(nextIndex);
+                if (mapItem.next != null) {
+                    duplicate = nextItem.equals(mapItem.next.value);
+                } else {
+                    throw new RuntimeException("此结果不需要参考，没有进行比较");
+                }
+            } else {
+                throw new RuntimeException("此结果不需要参考，没有进行比较");
+            }
+
+        }
+
+        return duplicate;
+    }
+
     public static void main(String[] args) {
-        List<String> originalList = Arrays.asList("A", "B", "C", "D", "E", "F", "G", "H", "H", "I", "J", "J", "K", "L", "L", "L", "M", "M", "M", "M");
+        List<String> originalList = Arrays.asList("A", "B", "C", "D", "E", "F", "G", "H", "H", "I", "J", "J", "K", "L", "L", "L", "M", "M", "M", "M", "N", "O", "P", "P", "P", "P", "P", "Q", "R", "S");
 
         List<String> sublist1 = Arrays.asList("A", "B", "C", "D", "E");
         List<String> sublist2 = Arrays.asList("B", "C", "D", "E", "F");
@@ -154,8 +277,15 @@ public class MergeLists {
         List<String> sublist8 = Arrays.asList("K", "L", "L");
         List<String> sublist9 = Arrays.asList("L", "L", "M", "M");
         List<String> sublist10 = Arrays.asList("M", "M", "M");
+        List<String> sublist11 = Arrays.asList("M", "N", "O", "P");
+        List<String> sublist12 = Arrays.asList("P", "P", "P", "P");
+        List<String> sublist13 = Arrays.asList("P", "Q", "R", "S");
+        List<String> sublist14 = Arrays.asList("H", "I", "J", "J", "K", "L");
         long startTime = System.currentTimeMillis();
-        List<String> mergedList = mergeLists(sublist1, sublist2, sublist3, sublist4, sublist5, sublist6, sublist7, sublist8, sublist9, sublist10);
+
+        // 创建双向链表的头节点
+        Node head = new Node("\0");
+        List<String> mergedList = mergeLists(head, sublist1, sublist2, sublist3, sublist4, sublist5, sublist6, sublist7, sublist8, sublist9, sublist10, sublist11, sublist12, sublist13, sublist14);
 
         System.out.println("Merged List: " + mergedList);
         System.out.println("耗时: " + (System.currentTimeMillis() - startTime) + "毫秒");
